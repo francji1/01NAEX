ef1e487d94f4153a63dabd92c7e4a9161e96d165
Jiří Franc <francji1@fjfi.cvut.cz>
2021-10-26 14:41:10 +0200

    Repair data source in Ex4

--- R/NAEX2021_Ex04.R
+++ R/NAEX2021_Ex04.R
@@ -14,11 +14,44 @@ library(alr3)
 library(FrF2)          #for 2^k  factorial design
 library(DoE.base)      # Full factorials, orthogonal arrays and base utilities for DoE packages
 # for opening xls files: library(gdata) library(XLConnect) library(xlsReadWrite)
+library(tidyverse)      # Full factorials, orthogonal arrays and base utilities for DoE packages
 
 
 # Solve Problems 4-23, 4.40, and 4-42 from Montgomery.
 
-Problem_4_23 <- read.table("Problem_4_23.txt",header=TRUE,sep=";")
-Problem_4_40 <- read.table("Problem_4_40.txt",header=TRUE,sep=";")
-Problem_4_42 <- read.table("Problem_4_42.txt",header=TRUE,sep=";")
+Problem_4_23 <- read.table("data/Problem_4_23.txt",header=TRUE,sep=";")
+Problem_4_40 <- read.table("data/Problem_4_40.txt",header=TRUE,sep=";")
+Problem_4_42 <- read.table("data/Problem_4_42.txt",header=TRUE,sep=";")
+
+Problem_4_23
+Problem_4_23 <- Problem_4_23 %>% 
+  mutate(Order= factor(Order),
+         Operator = factor(Operator))
+anova(lm(Time~Method+Order+Operator,Problem_4_23))
+
+
+Problem_4_40
+Problem_4_40 <- Problem_4_40 %>% 
+  mutate(Additive= factor(Additive),
+         Car = factor(Car))
+str(Problem_4_40)
+summary(aov(Mileage~Additive+Car+Error(Car),Problem_4_40))
+
+# Not this way
+anova(lm(Mileage~Car+Additive,Problem_4_40))
+anova(lm(Mileage~Additive+Car,Problem_4_40))
+
+
+Problem_4_42
+Problem_4_42 <- Problem_4_42 %>% 
+  mutate(Concentration = factor(Concentration ),
+         Days = factor(Days))
+summary(aov(Strength~Concentration+Days+Error(Days),Problem_4_42))
+
+# Not this way
+anova(lm(Strength~Concentration+Days,Problem_4_42))
+anova(lm(Strength~Days+Concentration,Problem_4_42))
+
+
+
 
